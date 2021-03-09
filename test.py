2b91c400a823f72f980f6834fa9496e8450443a9
Nika Avramchuk <nika.avramchuk@gmail.com>
2021-03-09 21:32:40 +0100

    create testMethod that change wasRun variable to 1

--- test.py
+++ test.py
@@ -1,13 +1,11 @@
 class WasRun:
     def __init__(self, name):
         self.wasRun= None
-
-    def testMethod(self):
-        self.wasRun= 1 
-
-    def run(self):
-        self.testMethod()    
-
+        self.name= name
+    
+    def testMethod (self):
+        self.wasRun= 1
+        
 test = WasRun("testMethod")
 print (test.wasRun)
 test.run()
