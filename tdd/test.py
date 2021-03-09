8adbb4c874185bad69aec3818b291a81ba8ed138
Nika Avramchuk <nika.avramchuk@gmail.com>
2021-03-09 23:26:33 +0100

    create the log string in WasRun class responsible for the registering the fact of calling the test method

--- tdd/test.py
+++ tdd/test.py
@@ -18,27 +18,22 @@ class WasRun(TestCase):
 
     def setUp(self):
         self.wasRun= None
-        self.wasSetUp= 1      
+        self.wasSetUp= 1 
+        self.log= "setUp "     
     
     def testMethod (self):
         self.wasRun= 1
+        self.log = self.log + "testMethod "
 
 
 class TestCaseTest(TestCase):
 
-    def setUp(self):
-        self.test = WasRun("testMethod")
+    def testTemplateMethod(self):
+        test = WasRun("testMethod")
+        test.run()
+        assert("setUp testMethod " == test.log)    
 
-    def testRunning(self):
-        self.test.run()
-        assert(self.test.wasRun)
-
-    def testSetUp(self):
-        self.test.run()
-        assert(self.test.wasSetUp)    
-
-TestCaseTest("testSetUp").run()
-TestCaseTest("testRunning").run()
+TestCaseTest("testTemplateMethod").run()
 
         
 
