a6503cb3854bbc455c5be325e69b595c7796e613
Nika Avramchuk <nika.avramchuk@gmail.com>
2021-03-09 22:29:55 +0100

    create test that check if test was set up and add setUp variable and method to the TestCase class

--- test.py
+++ test.py
@@ -2,27 +2,43 @@ class TestCase:
     def __init__(self, name):
         self.name= name
 
+    def setUp(self):
+        pass    
+
     def run(self):
+        self.setUp()
         method = getattr(self,self.name)
-        method()    
+        method()      
 
 class WasRun(TestCase):
     def __init__(self, name):
         self.wasRun= None
-        TestCase.__init__(self,name)
+        self.wasSetUp= None
+        TestCase.__init__(self,name)  
+
+    def setUp(self):
+        self.wasRun= None
+        self.wasSetUp= 1      
     
     def testMethod (self):
         self.wasRun= 1
+
 
 class TestCaseTest(TestCase):
 
     def testRunning(self):
         test = WasRun("testMethod")
-        assert(test.wasRun)
         test.run()
         assert(test.wasRun)
 
+    def testSetUp(self):
+        test = WasRun("testMethod")
+        test.run()
+        assert(test.wasSetUp)    
+
+TestCaseTest("testSetUp").run()
 TestCaseTest("testRunning").run()
+
         
 
 
