f828e5cd773d343521d801441f927b0158a6cd7a
Nika Avramchuk <nika.avramchuk@gmail.com>
2021-03-09 23:33:05 +0100

    Restructured the testing strategy from flags to a log and implement tearDown() using the new log

--- tdd/test.py
+++ tdd/test.py
@@ -3,27 +3,29 @@ class TestCase:
         self.name= name
 
     def setUp(self):
-        pass    
+        pass  
+
+    def tearDown(self):
+        pass  
 
     def run(self):
         self.setUp()
         method = getattr(self,self.name)
-        method()      
+        method()   
+        self.tearDown()   
 
 class WasRun(TestCase):
     def __init__(self, name):
-        self.wasRun= None
-        self.wasSetUp= None
         TestCase.__init__(self,name)  
 
     def setUp(self):
-        self.wasRun= None
-        self.wasSetUp= 1 
         self.log= "setUp "     
     
-    def testMethod (self):
-        self.wasRun= 1
+    def testMethod(self):
         self.log = self.log + "testMethod "
+
+    def tearDown(self):
+        self.log = self.log + "tearDown "    
 
 
 class TestCaseTest(TestCase):
@@ -31,7 +33,7 @@ class TestCaseTest(TestCase):
     def testTemplateMethod(self):
         test = WasRun("testMethod")
         test.run()
-        assert("setUp testMethod " == test.log)    
+        assert("setUp testMethod tearDown " == test.log)    
 
 TestCaseTest("testTemplateMethod").run()
 
